12f2c5a38d29941c96f143fcae5fabd5d1a5f1aa
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2023-11-23 02:05:15 +0700

    Added imageupload endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,12 +1,15 @@
 import express, { Router } from 'express';
 
 import { login, register, getDataFromToken } from '../controller/userController';
-
+import { uploadFile } from '../controller/auctionController';
 const router: Router = express.Router();
 
 //user endpoints
 router.post("/login", login);
 router.post("/register", register);
 router.get("/getDataFromToken", getDataFromToken);
+
+//test uploadfile
+router.post("/uploadFile", uploadFile);
 
 export default router;
